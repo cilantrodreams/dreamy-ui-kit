6e3b05705744f3b6e7795756fa9ca4abb39429cb
Thomas Ngo <thomasngo818@gmail.com>
2022-05-04 15:39:54 -0700

    Refactor story to pass menu items in as prop

--- src/components/Menu/__stores__/Menu.stories.jsx
+++ src/components/Menu/__stores__/Menu.stories.jsx
@@ -4,8 +4,23 @@ import Menu from "../Menu";
 
 const stories = storiesOf("Menu", module);
 
+const items = [
+  {
+    id: 1,
+    name: "Thomas",
+  },
+  {
+    id: 2,
+    name: "Projects",
+  },
+  {
+    id: 3,
+    name: "Blog",
+  },
+];
+
 stories.add("Menu", () => {
   return (
-    <Menu />
+    <Menu items={items} />
   );
 });
