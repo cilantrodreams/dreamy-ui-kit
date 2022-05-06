b69f0273a6ad69ea632d763ba0fd9d1b0140e195
Thomas Ngo <thomasngo818@gmail.com>
2022-05-05 23:38:28 -0700

    Add global variable for font

--- src/shared/styles.js
+++ src/shared/styles.js
@@ -1,5 +1,6 @@
 import { css } from "styled-components";
 
+// global style variables
 export const color = {
   primary1: "#B85042", // muted scarlet
   primary2: "#E7E8D1", // light olive
@@ -36,3 +37,28 @@ export const pageMargins = css`
     margin: 0 ${pageMargin * 4}%;
   }
 `;
+
+export const typography = {
+  type: {
+    primary: '"Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif',
+    code: '"SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace',
+  },
+  weight: {
+    regular: "400",
+    bold: "700",
+    extrabold: "800",
+    black: "900",
+  },
+  size: {
+    s1: "12",
+    s2: "14",
+    s3: "16",
+    m1: "20",
+    m2: "24",
+    m3: "28",
+    l1: "32",
+    l2: "40",
+    l3: "48",
+    code: "90",
+  },
+};
