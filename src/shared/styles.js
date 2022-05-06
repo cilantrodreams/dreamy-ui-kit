cbd8329dfed99522295ca333071c0b78a5f048ba
Thomas Ngo <thomasngo818@gmail.com>
2022-05-05 23:35:14 -0700

    Add global styles for page margin at different breakpoints

--- src/shared/styles.js
+++ src/shared/styles.js
@@ -17,3 +17,22 @@ export const spacing = {
     default: 10,
   },
 };
+
+export const breakpoint = 600;
+export const pageMargin = "5.55555";
+
+export const pageMargins = css`
+  padding: 0 ${spacing.padding.medium}px;
+  @media (min-width: ${breakpoint * 1}px) {
+    margin: 0 ${pageMargin * 1}%;
+  }
+  @media (min-width: ${breakpoint * 2}px) {
+    margin: 0 ${pageMargin * 2}%;
+  }
+  @media (min-width: ${breakpoint * 3}px) {
+    margin: 0 ${pageMargin * 3}%;
+  }
+  @media (min-width: ${breakpoint * 4}px) {
+    margin: 0 ${pageMargin * 4}%;
+  }
+`;
